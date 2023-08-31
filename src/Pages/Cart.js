598da60fe9fece05822251844ea32d1247a2c263
harish242 <markollh242@gmail.com>
2023-08-31 15:08:53 +0530

    dgfdh

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,20 +1,21 @@
-import * as React from "react";
+import React, { useEffect } from "react";
+import { CartState } from "../component/Home";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import { CartState } from "../component/Home";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Emptycard from '../component/Emptycard'
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import Modals from "../component/Modal";
-import '../styles/Cart.css'
+import '../styles/Cart.css';
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -35,87 +36,83 @@ export default function FixedContainer() {
   const cost = selectedProducts.reduce((acc, curr) => {
     return acc + curr.count * curr.price;
   }, 0);
+
   return (
-    <React.Fragment>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          {selectedProducts.length===0?<Emptycard/>:selectedProducts.map((item) => {
-            return (
-              <Item key={item.id} style={{margin:'20px'}}>
-                <ListItem>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        {/* Render the selected products */}
+        {selectedProducts.length === 0 ? (
+          <Emptycard />
+        ) : (
+          selectedProducts.map((item) => (
+            <Item key={item.id} style={{ margin: '20px' }}>
+              {/* List item details */}
+              <ListItem>
                 <ListItemAvatar>
                   <Avatar alt="Remy Sharp" src={item.images[0]} />
                 </ListItemAvatar>
-                <ListItemText  secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: 'inline' }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                {item.title.slice(0, 10)}
-              </Typography>
-              {`-${item.description.slice(0,40)}`}
-            </React.Fragment>
-          }></ListItemText>
-                </ListItem>
-               
-                <div style={{display:'flex'}} className='flex-container'>
+                <ListItemText
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {item.title.slice(0, 10)}
+                      </Typography>
+                      {`-${item.description.slice(0, 40)}`}
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+
+              {/* Quantity and action controls */}
+              <div style={{ display: 'flex' }} className='flex-container'>
                 <ListItem>Qty:</ListItem>
                 <ListItem>
                   <RemoveIcon onClick={() => onSelectRemove(item)} />
-
                 </ListItem>
                 <ListItem>{item.count}</ListItem>
                 <ListItem>
-                  {/* <RemoveIcon onClick={() => onSelectRemove(item)} /> */}
                   <AddIcon onClick={() => onSelect(item)} />
-
                 </ListItem>
-
-                {/* <ListItem>{`Rate:${item.rating.rate}`}</ListItem> */}
                 <ListItem>
                   <DeleteOutlineIcon onClick={() => HandleDelete(item)} />
                 </ListItem>
-                <span>cost:{item.count*item.price}</span>
-                </div>              
+                <span>cost:{item.count * item.price}</span>
+              </div>
 
-                <Divider variant="inset" component="li" />
-              </Item>
-            );
-          })}
-        </Grid>
-        <Grid item xs={12} sm={6} >
-          <Item
-            sx={{
-              bgcolor: "#FFECEC",
-              height: "85vh",
-              color: "white",
-              marginTop: "20px",
-              backgroundImage:"url('https://img.freepik.com/free-photo/orange-copy-space-background-with-sale-idea_23-2148305925.jpg?w=1060&t=st=1688323597~exp=1688324197~hmac=7c556ad5f2f5cda8dc5c6daec22ca67890bd994b4d4900b19c76a9348a45f533')",
-              backgroundRepeat:'no-repeat',
-              backgroundSize:'cover',
-              "@media (max-width: 600px)": {
-                backgroundImage: "url('https://img.freepik.com/free-photo/orange-copy-space-background-with-sale-idea_23-2148305925.jpg?w=1060&t=st=1688323597~exp=1688324197~hmac=7c556ad5f2f5cda8dc5c6daec22ca67890bd994b4d4900b19c76a9348a45f533')",
-                // Adjust other styles as needed
-              },
-            }}
-          >
-            <div
-              style={{ fontSize: "30px", color: "black", marginTop: "130px",marginLeft:'300px',fontWeight:'bolder' }}
-            >
-              Total items:{totalSelected}
-            </div>
-            <br />
-            <br />
-            <div style={{ fontSize: "20px", color: "blac",fontWeight:'bold',marginLeft:'300px' }}>
-              Total Cost:${cost.toFixed(2)}
-            </div>          
-            <Modals style={{marginTop:'30px'}} />
-          </Item>
-        </Grid>
+              <Divider variant="inset" component="li" />
+            </Item>
+          ))
+        )}
       </Grid>
-    </React.Fragment>
+      <Grid item xs={12} md={6}>
+        {/* Total summary */}
+        <Item
+          sx={{
+            bgcolor: "#FFECEC",
+            height: "85vh",
+            color: "white",
+            marginTop: "20px",
+            backgroundImage: "url('your-image-url')",  // Replace with your image URL
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+          }}
+        >
+          <div style={{ fontSize: "30px", color: "black", marginTop: "130px", marginLeft: '300px', fontWeight: 'bolder' }}>
+            Total items:{totalSelected}
+          </div>
+          <br />
+          <br />
+          <div style={{ fontSize: "20px", color: "black", fontWeight: 'bold', marginLeft: '300px' }}>
+            Total Cost:${cost.toFixed(2)}
+          </div>
+          <Modals style={{ marginTop: '30px' }} />
+        </Item>
+      </Grid>
+    </Grid>
   );
 }

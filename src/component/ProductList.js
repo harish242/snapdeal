cb60309771a389c4b448049f26d50382460e1275
harish242 <markollh242@gmail.com>
2023-09-05 01:48:43 +0530

    flkhjflh

--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -298,7 +298,7 @@ navigate('/Home')
       <Typography variant="body2" color="text.secondary">
           {/* Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica */}
-         New user?Register
+         Happy Shopping💕
         </Typography>
       </div>
      
